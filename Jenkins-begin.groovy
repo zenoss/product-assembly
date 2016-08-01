586ac3d06a8dd3a0e43b259e39dc450823dad8d2
Glenn Jones <gjones@zenoss.com>
2016-08-01 13:54:41 -0500

    Update git-commit

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -4,12 +4,12 @@ node{
 
     stage 'Checkout product-assembly repo'
         //git branch: 'develop', credentialsId: '6ece10bd-11c1-4e23-8f36-6848f6c4c704', url: 'https://github.com/zenoss/product-assembly'
-        println("Got commit=#${GIT_COMMIT}")
+        echo env.GIT_COMMIT
 
-        //sshagent(['6ece10bd-11c1-4e23-8f36-6848f6c4c704']) {
-        //    GIT_SHA=sh("git rev-parse HEAD")
-        //    println("Got sha=#${GIT_SHA}")
-        //}
+        sshagent(['6ece10bd-11c1-4e23-8f36-6848f6c4c704']) {
+            sh("git rev-parse HEAD >git_sha.id")
+            println("Got sha=#${GIT_SHA}")
+        }
 
     stage 'Build product-base'
         sh("MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
