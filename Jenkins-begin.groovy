d000ec214efcebf3e51a3f6133ab3f72e8312cc5
Glenn Jones <gjones@zenoss.com>
2016-08-01 15:17:30 -0500

    more changes

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -1,10 +1,15 @@
+//
+// The Jenkins job parameters for this script are:
+//    MATURITY          - the image maturity level (e.g. 'unstable', 'testing', 'stable')
+//    GIT_CREDENTIAL_ID - the UUID of the GIT credentials used to checkout stuff from github
+//
 node ('build-ubuntu') {
     PRODUCT_BUILD_NUMBER=env.BUILD_NUMBER
     currentBuild.displayName = "product bld #${PRODUCT_BUILD_NUMBER}"
 
     stage 'Checkout product-assembly repo'
         // FIXME: parameterize the git credentialsID
-        git branch: 'develop', credentialsId: '796808e0-a2b9-4b66-88d6-1ce283234ad1', url: 'https://github.com/zenoss/product-assembly'
+        git branch: 'develop', credentialsId: '${GIT_CREDENTIAL_ID}', url: 'https://github.com/zenoss/product-assembly'
 
         // Record the current git commit id in the variable 'git_sha'
         sh("git rev-parse HEAD >git_sha.id")
@@ -12,25 +17,27 @@ node ('build-ubuntu') {
         println("Building from git commit='${git_sha}' for MATURITY='${MATURITY}'")
 
     stage 'Build product-base'
-        echo env.WORKSPACE
-        sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
+        sh("pwd;cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
+
+    stage 'Push product-base'
+        sh("pwd;cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push")
 
     stage 'Build all products'
         def branches = [
             'core': {
                 println "Starting core-pipeline"
                 build job: 'core-pipeline', parameters: [
+                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                     [$class: 'ChoiceParameterValue', name: 'MATURITY', value: MATURITY],
                     [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                 ]
             },
             'resmgr': {
                 println "Starting resmgr-pipeline"
                 build job: 'resmgr-pipeline', parameters: [
+                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                     [$class: 'ChoiceParameterValue', name: 'MATURITY', value: MATURITY],
                     [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                 ]
             },
         ]
