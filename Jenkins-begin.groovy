69ee4480cc2f5179d0240edc3cd61cba5bf2ac08
Glenn Jones <gjones@zenoss.com>
2016-08-02 09:07:53 -0500

    add some build doc

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -1,46 +1,52 @@
 #!groovy
 //
+// Jenkins-begin.groovy - Jenkins script for initiating the Zenoss product build process.
+//
 // The Jenkins job parameters for this script are:
+//
+//    BRANCH_NAME       - the name of the GIT branch to build from
+//    GIT_CREDENTIAL_ID - the UUID of the Jenkins GIT credentials used to checkout stuff from github
 //    MATURITY          - the image maturity level (e.g. 'unstable', 'testing', 'stable')
-//    GIT_CREDENTIAL_ID - the UUID of the GIT credentials used to checkout stuff from github
 //
 node ('build-ubuntu') {
+    // To avoid naming confusion with downstream jobs that have their own BUILD_NUMBER variables,
+    // define 'PRODUCT_BUILD_NUMBER' as the parameter name that will be used by all downstream
+    // jobs to identify a particular execution of the build pipeline.
     PRODUCT_BUILD_NUMBER=env.BUILD_NUMBER
     currentBuild.displayName = "product build #${PRODUCT_BUILD_NUMBER}"
 
     stage 'Checkout product-assembly repo'
-        // FIXME: find a way to pass BRANCH_NAME to the git dsl
+        // FIXME: find a way to pass BRANCH_NAME to the git dsl so we don't have to edit this
+        //        script for each new support/N.n.x branch.
         git branch: 'develop', credentialsId: '${GIT_CREDENTIAL_ID}', url: 'https://github.com/zenoss/product-assembly'
 
-        // Record the current git commit id in the variable 'git_sha'
+        // Record the current git commit sha in the variable 'GIT_SHA'
         sh("git rev-parse HEAD >git_sha.id")
         GIT_SHA=readFile('git_sha.id').trim()
         println("Building from git commit='${GIT_SHA}' for MATURITY='${MATURITY}'")
 
     stage 'Build product-base'
-        sh("pwd;cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
+        sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
 
     stage 'Push product-base'
-        sh("pwd;cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push")
+        sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push")
 
     stage 'Build all product pipelines'
         def branches = [
             'core-pipeline': {
-                println "Starting core-pipeline"
                 build job: 'core-pipeline', parameters: [
                     [$class: 'StringParameterValue', name: 'BRANCH_NAME', value: BRANCH_NAME],
-                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                     [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
+                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                     [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
                     [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
                 ]
             },
             'resmgr-pipeline': {
-                println "Starting resmgr-pipeline"
                 build job: 'resmgr-pipeline', parameters: [
                     [$class: 'StringParameterValue', name: 'BRANCH_NAME', value: BRANCH_NAME],
-                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                     [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
+                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
                     [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
                     [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
                 ]
--- Jenkins-image.groovy
+++ Jenkins-image.groovy
@@ -1,11 +1,22 @@
 #!groovy
+//
+// Jenkins-image.groovy - Jenkins script for building a single Zenoss product image.
+//
+// The Jenkins job parameters for this script are:
+//
+//    GIT_SHA              - the GIT SHA of product-assembly to checkout for this build
+//    GIT_CREDENTIAL_ID    - the UUID of the Jenkins GIT credentials used to checkout stuff from github
+//    MATURITY             - the image maturity level (e.g. 'unstable', 'testing', 'stable')
+//    PRODUCT_BUIlD_NUMBER - the build number for any given execution of this build pipeline; set by the begin job.
+//    TARGET_PRODUCT       - identifies the target product (e.g. 'core', 'resmgr', 'ucspm', etc)
+//
 node ('build-ubuntu') {
     currentBuild.displayName = "product build #${PRODUCT_BUILD_NUMBER}"
 
     stage 'Build image'
-        echo "Buiding image ..."
-        sh("pwd")
-        git branch: 'develop', credentialsId: '${GIT_CREDENTIAL_ID}', url: 'https://github.com/zenoss/product-assembly'
+        // NOTE: The 'master' branch name here is only used to clone the github repo.
+        //       The next checkout command will align the build with the correct target revision.
+        git branch: 'master', credentialsId: '${GIT_CREDENTIAL_ID}', url: 'https://github.com/zenoss/product-assembly'
         sh("git checkout ${GIT_SHA}")
         sh("cd ${TARGET_PRODUCT};MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
 
@@ -16,5 +27,5 @@ node ('build-ubuntu') {
     // build svc def rpm
     // push svc-dev rpm
     stage 'Compile service definitions'
-       echo 'Hello World 2'
+       echo 'TODO - compile service defs'
 }
