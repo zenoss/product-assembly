ab72653f972b837cf6d0c9266c63d58c70969c71
jplouis <jplouis@gmail.com>
2016-08-23 09:17:37 -0500

    bash script for generating repos file

---
+++ repos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    FILE=".repos.json"
+else
+    FILE=$1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+${DIR}/artifact_download.py ${DIR}/component_versions.json --git_output ${FILE} ;${DIR}/artifact_download.py ${DIR}/zenpack_versions.json --git_output ${FILE} --append
