d9b1fc818e4211e7ea9baf26783f9b2c7f0ec678
jplouis <jplouis@gmail.com>
2016-11-16 12:01:46 -0600

    fix pinned conditional

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -24,7 +24,7 @@ node ('build-zenoss-product') {
         println("Building from git commit='${GIT_SHA}' on branch ${BRANCH} for MATURITY='${MATURITY}'")
 
     stage 'Build product-base'
-        if (PINNED) {
+        if (PINNED == true) {
             // make sure SVCDEF_GIT_REF has is of the form x.x.x, where x is an integer
             sh("grep ^SVCDEF_GIT_REF=[0-9]\\{1,\\}\\.[0-9]\\{1,\\}\\.[0-9]\\{1,\\} versions.mk")
             sh("./artifact_download.py component_versions.json --pinned")
