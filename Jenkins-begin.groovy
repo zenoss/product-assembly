85c63b543f40f2cdfd9b2b924f3d433fcf2a4b62
Glenn Jones <gjones@zenoss.com>
2017-02-20 15:05:15 -0600

    Add try/finally to generate HTML report for begin jobs

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -15,47 +15,52 @@ node ('build-zenoss-product') {
     PRODUCT_BUILD_NUMBER=env.BUILD_NUMBER
     currentBuild.displayName = "product build #${PRODUCT_BUILD_NUMBER}"
 
-    stage 'Checkout product-assembly repo'
-        // Make sure we start in a clean directory to ensure a fresh git clone
-        deleteDir()
-        git branch: BRANCH, credentialsId: GIT_CREDENTIAL_ID, url: 'https://github.com/zenoss/product-assembly'
+    try {
+        stage 'Checkout product-assembly repo'
+            // Make sure we start in a clean directory to ensure a fresh git clone
+            deleteDir()
+            git branch: BRANCH, credentialsId: GIT_CREDENTIAL_ID, url: 'https://github.com/zenoss/product-assembly'
 
-        // Record the current git commit sha in the variable 'GIT_SHA'
-        sh("git rev-parse HEAD >git_sha.id")
-        GIT_SHA=readFile('git_sha.id').trim()
-        println("Building from git commit='${GIT_SHA}' on branch ${BRANCH} for MATURITY='${MATURITY}'")
+            // Record the current git commit sha in the variable 'GIT_SHA'
+            sh("git rev-parse HEAD >git_sha.id")
+            GIT_SHA=readFile('git_sha.id').trim()
+            println("Building from git commit='${GIT_SHA}' on branch ${BRANCH} for MATURITY='${MATURITY}'")
 
-    stage 'Build product-base'
-        if (PINNED == "true") {
-            // make sure SVCDEF_GIT_REF has is of the form x.x.x, where x is an integer
-            sh("grep '^SVCDEF_GIT_REF=[0-9]\\{1,\\}\\.[0-9]\\{1,\\}\\.[0-9]\\{1,\\}' versions.mk")
-            sh("./artifact_download.py component_versions.json --pinned")
-            sh("./artifact_download.py zenpack_versions.json --pinned")
-        }
-        sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
+        stage 'Build product-base'
+            if (PINNED == "true") {
+                // make sure SVCDEF_GIT_REF has is of the form x.x.x, where x is an integer
+                sh("grep '^SVCDEF_GIT_REF=[0-9]\\{1,\\}\\.[0-9]\\{1,\\}\\.[0-9]\\{1,\\}' versions.mk")
+                sh("./artifact_download.py component_versions.json --pinned")
+                sh("./artifact_download.py zenpack_versions.json --pinned")
+            }
+            sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make clean build")
 
-    stage 'Push product-base'
-        sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push clean")
+        stage 'Push product-base'
+            sh("cd product-base;MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push clean")
 
-    stage 'Run all product pipelines'
-        def branches = [
-            'core-pipeline': {
-                build job: 'core-pipeline', parameters: [
-                    [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
-                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
-                    [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
-                    [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                ]
-            },
-            'resmgr-pipeline': {
-                build job: 'resmgr-pipeline', parameters: [
-                    [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
-                    [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
-                    [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
-                    [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                ]
-            },
-        ]
+        stage 'Run all product pipelines'
+            def branches = [
+                'core-pipeline': {
+                    build job: 'core-pipeline', parameters: [
+                        [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
+                        [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
+                        [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
+                        [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
+                    ]
+                },
+                'resmgr-pipeline': {
+                    build job: 'resmgr-pipeline', parameters: [
+                        [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
+                        [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
+                        [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
+                        [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
+                    ]
+                },
+            ]
 
-        parallel branches
-}
+            parallel branches
+    } finally {
+        sh("./build_status.py -b ${BRANCH} -p ${PRODUCT_BUILD_NUMBER} -html buildReport.html")
+        publishHTML([allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: './', reportFiles: 'buildReport.html', reportName: 'Build Summary Report'])
+        archive includes: 'buildReport.*'
+    }
