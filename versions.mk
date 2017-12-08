6a6475154e02ae9994f01c2a7cfdd312c58c3397
jplouis <jplouis@gmail.com>
2017-12-08 14:10:10 -0600

    bump to 6.2

--- versions.mk
+++ versions.mk
@@ -14,9 +14,9 @@
 # VERSION           the full Zenoss product version; e.g. 5.2.0
 # UCSPM_VERSION     the version of the ucspm release; e.g 2.1.0
 #
-SHORT_VERSION=6.1
+SHORT_VERSION=6.2
 SVCDEF_GIT_REF=develop
-VERSION=6.1.0
+VERSION=6.2.0
 UCSPM_VERSION=3.0.0
 VERSION_TAG=1
 
