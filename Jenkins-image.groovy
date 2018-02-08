95c684e5719070297e4a8961fe88cecdc2b8f8db
jplouis <jplouis@gmail.com>
2018-02-08 12:39:14 -0600

    WIP remove appliance build for cse

--- Jenkins-image.groovy
+++ Jenkins-image.groovy
@@ -38,6 +38,7 @@ node ('build-zenoss-product') {
         SERVICED_MATURITY=versionProps['SERVICED_MATURITY']
         SERVICED_VERSION=versionProps['SERVICED_VERSION']
         SERVICED_BUILD_NUMBER=versionProps['SERVICED_BUILD_NUMBER']
+        SHORT_VERSION=versionProps['SHORT_VERSION']
         echo "SVCDEF_GIT_REF=${SVCDEF_GIT_REF}"
         echo "ZENOSS_VERSION=${ZENOSS_VERSION}"
         echo "SERVICED_BRANCH=${SERVICED_BRANCH}"
@@ -53,11 +54,11 @@ node ('build-zenoss-product') {
     }
 
     stage ('Test image') {
-        sh("cd ${TARGET_PRODUCT};MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make run-tests")
+//        sh("cd ${TARGET_PRODUCT};MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make run-tests")
     }
 
     stage ('Push image') {
-        sh("cd ${TARGET_PRODUCT};MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push clean")
+//        sh("cd ${TARGET_PRODUCT};MATURITY=${MATURITY} BUILD_NUMBER=${PRODUCT_BUILD_NUMBER} make push clean")
     }
 
     stage ('Compile service definitions and build RPM') {
@@ -86,70 +87,19 @@ node ('build-zenoss-product') {
         archive includes: 'svcdefs/build/zenoss-service/output/**'
     }
 
-    stage ('Push RPM') {
-        // FIXME - if we never use the pipeline to build/publish artifacts directly to the stable or
-        //         testing repos, then maybe we should remove MATURITY as an argument for this job?
-        def s3Subdirectory = "/yum/zenoss/" + MATURITY + "/centos/el7/os/x86_64"
-        def rpmJobLabel = TARGET_PRODUCT + " product build #" + PRODUCT_BUILD_NUMBER
-        build job: 'rpm_repo_push', parameters: [
-            [$class: 'StringParameterValue', name: 'JOB_LABEL', value: rpmJobLabel],
-            [$class: 'StringParameterValue', name: 'UPSTREAM_JOB_NAME', value: pipelineBuildName],
-            [$class: 'StringParameterValue', name: 'S3_BUCKET', value: 'get.zenoss.io'],
-            [$class: 'StringParameterValue', name: 'S3_SUBDIR', value: s3Subdirectory]
-        ]
+    stage ('Upload service definitions') {
+        def archiveEnv = "SHORT_VERSION=${SHORT_VERSION}\
+            ZENOSS_VERSION=${ZENOSS_VERSION}\
+            TARGET_PRODUCT=${TARGET_PRODUCT} \
+            MATURITY=${MATURITY}
+            BUILD_NUMBER=${PRODUCT_BUILD_NUMBER}"
+        sh("${archiveEnv} python archive.py --service-def")
+    }
+    stage ('Build offline images') {
+
+    }
+    stage ('Upload offline image') {
+        sh("python archive.py --offline-images")
     }
 
-    stage ('Build Appliances') {
-
-        def branches = [:]
-
-        if (TARGET_PRODUCT == "resmgr") {
-            // After building RM, we build two sets of appliances; one for ZSD and another for POC
-
-            // We have to use this version of the for-loop and _not_ the for(String s: strings)
-            // as per https://jenkins.io/doc/pipeline/examples/#parallel-from-list
-            def appliances = ["zsd", "poc"]
-            for (int i = 0; i < appliances.size(); i++) {
-                def applianceTarget = appliances.get(i);
-                def jobLabel = applianceTarget + " appliance for " + TARGET_PRODUCT + " product build #" + PRODUCT_BUILD_NUMBER
-                def branch = {
-                    build job: 'appliance-build', parameters: [
-                            [$class: 'StringParameterValue', name: 'JOB_LABEL', value: jobLabel],
-                            [$class: 'StringParameterValue', name: 'TARGET_PRODUCT', value: applianceTarget],
-                            [$class: 'StringParameterValue', name: 'BRANCH', value: BRANCH],
-                            [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                            [$class: 'StringParameterValue', name: 'ZENOSS_MATURITY', value: MATURITY],
-                            [$class: 'StringParameterValue', name: 'ZENOSS_VERSION', value: ZENOSS_VERSION],
-                            [$class: 'StringParameterValue', name: 'SERVICED_BRANCH', value: SERVICED_BRANCH],
-                            [$class: 'StringParameterValue', name: 'SERVICED_MATURITY', value: SERVICED_MATURITY],
-                            [$class: 'StringParameterValue', name: 'SERVICED_VERSION', value: SERVICED_VERSION],
-                            [$class: 'StringParameterValue', name: 'SERVICED_BUILD_NUMBER', value: SERVICED_BUILD_NUMBER],
-                            [$class: 'BooleanParameterValue', name: 'BUILD_APPLIANCES', value: BUILD_APPLIANCES.toBoolean()],
-
-                    ]
-                }
-
-                branches[applianceTarget] = branch
-            }
-        } else {
-            def jobLabel = TARGET_PRODUCT + " appliance for product build #" + PRODUCT_BUILD_NUMBER
-            branches[TARGET_PRODUCT] = {
-                build job: 'appliance-build', parameters: [
-                        [$class: 'StringParameterValue', name: 'JOB_LABEL', value: jobLabel],
-                        [$class: 'StringParameterValue', name: 'TARGET_PRODUCT', value: TARGET_PRODUCT],
-                        [$class: 'StringParameterValue', name: 'BRANCH', value: BRANCH],
-                        [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                        [$class: 'StringParameterValue', name: 'ZENOSS_MATURITY', value: MATURITY],
-                        [$class: 'StringParameterValue', name: 'ZENOSS_VERSION', value: ZENOSS_VERSION],
-                        [$class: 'StringParameterValue', name: 'SERVICED_BRANCH', value: SERVICED_BRANCH],
-                        [$class: 'StringParameterValue', name: 'SERVICED_MATURITY', value: SERVICED_MATURITY],
-                        [$class: 'StringParameterValue', name: 'SERVICED_VERSION', value: SERVICED_VERSION],
-                        [$class: 'StringParameterValue', name: 'SERVICED_BUILD_NUMBER', value: SERVICED_BUILD_NUMBER],
-                        [$class: 'BooleanParameterValue', name: 'BUILD_APPLIANCES', value: BUILD_APPLIANCES.toBoolean()],
-                ]
-            }
-        }
-
-        parallel branches
-    }
 }
