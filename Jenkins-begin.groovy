da1aa6872047301ab357ea0f102cf0ebf201a8a0
Jason Peacock <jpeacock@zenoss.com>
2018-04-24 07:24:03 -0500

    Disable the uscpm pipeline build

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -68,16 +68,16 @@ node ('build-zenoss-product') {
                         [$class: 'BooleanParameterValue', name: 'BUILD_APPLIANCES', value: BUILD_APPLIANCES.toBoolean()],
                     ]
                 },
-                'ucspm-pipeline': {
-                    build job: 'ucspm-pipeline', parameters: [
-                        [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
-                        [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
-                        [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
-                        [$class: 'StringParameterValue', name: 'BRANCH', value: BRANCH],
-                        [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
-                        [$class: 'BooleanParameterValue', name: 'BUILD_APPLIANCES', value: BUILD_APPLIANCES.toBoolean()],
-                    ]
-                },
+                // 'ucspm-pipeline': {
+                //     build job: 'ucspm-pipeline', parameters: [
+                //         [$class: 'StringParameterValue', name: 'GIT_CREDENTIAL_ID', value: GIT_CREDENTIAL_ID],
+                //         [$class: 'StringParameterValue', name: 'GIT_SHA', value: GIT_SHA],
+                //         [$class: 'StringParameterValue', name: 'MATURITY', value: MATURITY],
+                //         [$class: 'StringParameterValue', name: 'BRANCH', value: BRANCH],
+                //         [$class: 'StringParameterValue', name: 'PRODUCT_BUILD_NUMBER', value: PRODUCT_BUILD_NUMBER],
+                //         [$class: 'BooleanParameterValue', name: 'BUILD_APPLIANCES', value: BUILD_APPLIANCES.toBoolean()],
+                //     ]
+                // },
             ]
 
             parallel branches
