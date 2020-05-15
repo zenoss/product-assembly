9aef59d4cb0d132deafd8408d0bd8be37cd0640b
Jason Peacock <jpeacock@zenoss.com>
2020-05-15 08:56:44 -0500

    Support migrations to MariaDB 10.3 image.
    Also installs the service migration package provided by zenoss-service.
    
    Fixes ZEN-32769.

--- Jenkins-begin.groovy
+++ Jenkins-begin.groovy
@@ -19,6 +19,7 @@ node('build-zenoss-product') {
     def JOB_NAME = env.JOB_NAME        // e.g. product-assembly/support-6.0.x/begin
     def JOB_URL = env.JOB_URL          // e.g. ${JENKINS_URL}job/${JOB_NAME}/
     def BUILD_URL = env.BUILD_URL      // e.g. ${JOB_URL}${PRODUCT_BUILD_NUMBER}
+    def SVCDEF_GIT_REF = ''
 
     currentBuild.displayName = "product build #${PRODUCT_BUILD_NUMBER} @${env.NODE_NAME}"
 
@@ -38,6 +39,43 @@ node('build-zenoss-product') {
                 sh("grep '^SVCDEF_GIT_REF=[0-9]\\{1,\\}\\.[0-9]\\{1,\\}\\.[0-9]\\{1,\\}' versions.mk")
                 sh("./artifact_download.py component_versions.json --pinned")
                 sh("./artifact_download.py zenpack_versions.json --pinned")
+            }
+            
+            def versionProps = readProperties file: 'versions.mk'
+            SVCDEF_GIT_REF = versionProps['SVCDEF_GIT_REF']
+        }
+
+        stage('Build Service Migrations') {
+            dir("svcdefs") {
+                // Run the checkout in a separate directory.
+                // We have to clean it ourselves, because Jenkins doesn't (apparently)
+                sh("make clean")
+                def repo_dir = sh(returnStdout: true, script: "make repo_dir").trim()
+                dir("${repo_dir}") {
+                    echo "Cloning zenoss-service - ${SVCDEF_GIT_REF} with credentialsId=${GIT_CREDENTIAL_ID}"
+                    // NOTE: The 'master' branch name here is only used to clone the github repo.
+                    //       The next checkout command will align the build with the correct target revision.
+                    git(
+                        branch: 'master',
+                        credentialsId: '${GIT_CREDENTIAL_ID}',
+                        url: 'https://github.com/zenoss/zenoss-service.git',
+                    )
+                    sh("git checkout ${SVCDEF_GIT_REF}")
+
+                    // Log the current SHA of zenoss-service so, when building from a branch,
+                    // we know exactly which commit went into a particular build
+                    def HEAD_SHA = sh(returnStdout: true, script: "git rev-parse HEAD")
+                    echo "zenoss/zenoss-service git SHA = ${HEAD_SHA}"
+                }
+
+                def makeArgs = [
+                    "BUILD_NUMBER=${PRODUCT_BUILD_NUMBER}",
+                    "IMAGE_NUMBER=${PRODUCT_BUILD_NUMBER}",
+                    "MATURITY=${MATURITY}",
+                ].join(' ')
+                sh("make migrations ${makeArgs}")
+
+                archiveArtifacts artifacts: "*.whl"
             }
         }
 
