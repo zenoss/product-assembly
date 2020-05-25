f1657b49c96bcaa10a3c0519da365ff93b8d0324
Roman Bakaleyko <roman.bakaleyko@gmail.com>
2020-05-25 20:10:44 +0800

    ZEN-32813 fix several  issues that came up after moving to new mariad… (#1133)
    * ZEN-32813 fix several  issues that came up after moving to new mariadb and relstorage
    
    * devimg build doesn't need migration package.
    
    Part of fixing ZEN-32813.
    
    Co-authored-by: Jason Peacock <jpeacock@zenoss.com>

--- mariadb-base/upgrade_database.sh
+++ mariadb-base/upgrade_database.sh
@@ -1,5 +1,22 @@
 #/bin/bash
 
+SERVICE=$1
+GLOBAL_CFG="/opt/zenoss/etc/global.conf"
+ADMIN_USER="root"
+PASSWD=""
+
+
+case $SERVICE in
+	mariadb-model)
+		ADMIN_USER=$(grep -r "zodb-admin-user" $GLOBAL_CFG | awk '{print $2}')
+		PASSWD=$(grep -r "zodb-admin-password" $GLOBAL_CFG | awk '{print $2}')
+		;;
+	mariadb-events)
+		ADMIN_USER=$(grep -r "zep-admin-user" $GLOBAL_CFG | awk '{print $2}')
+		PASSWD=$(grep -r "zep-admin-password" $GLOBAL_CFG | awk '{print $2}')
+		;;
+esac
+
 start_db() {
 	mysqld_safe --skip-syslog --log-error=/var/log/mysql/upgrade.log &
 	until mysqladmin ping 2>/dev/null; do
@@ -21,4 +38,5 @@ trap cleanup EXIT
 set -e
 
 start_db
-mysql_upgrade
+chown -R mysql:mysql /var/lib/mysql
+mysql_upgrade -u $USERNAME -p $PASSWD
