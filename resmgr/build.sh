f5b23a92958c9ae6f98eb5f133bf4c5804a0397a
Jason Peacock <jpeacock@zenoss.com>
2021-01-15 12:31:35 -0600

    Install ActiveState Python for resmgr image.
    Fixes ZEN-32951.

--- resmgr/build.sh
+++ resmgr/build.sh
@@ -7,6 +7,15 @@ create_build_containers
 start_build_containers
 copy_files
 create_zenoss
+
+echo
+echo "Installing ActiveState Python"
+docker container exec \
+	-w /src \
+	${product_name} \
+	/bin/bash -l -c "./install-activepython.sh" \
+	|| fail "Could not execute install-activepython.sh script"
+
 copy_upgrade_scripts
 stop_containers
 commit_images
