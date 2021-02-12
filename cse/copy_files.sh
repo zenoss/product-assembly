878e49820ce07ff99ce52bd531dc5a4ef7f0ce00
emiller <sockterrier@gmail.com>
2021-02-12 16:10:23 -0600

    hc script added

--- cse/copy_files.sh
+++ cse/copy_files.sh
@@ -6,5 +6,8 @@ cp /src/licenses.cse.html /opt/zenoss/Products/ZenUI3/docs/licenses.html
 cp /src/zenpacks.json /opt/zenoss/install_scripts/
 cp /src/zenpacks_artifact.log /opt/zenoss/log/
 
+mkdir -p /opt/zenoss/bin/healthchecks
+cp /src/zing_connector_answering /opt/zenoss/bin/healthchecks/
+
 mkdir -p /opt/zenoss/packs
 cp /src/zenpacks/* /opt/zenoss/packs/
