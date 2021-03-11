73accef3bca9523d803c9c17d0d6e8fa9e7db5b5
Jason Peacock <jpeacock@zenoss.com>
2021-03-11 10:17:34 -0600

    Rework mariadb-base image and squash it

--- mariadb-base/initialize_db.sh
+++ mariadb-base/initialize_db.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-SQL_COMMON_IN=/home/zenoss/permissions_common.sql.in
-SQL_DB_IN=/home/zenoss/permissions.sql.in
 SQL_PERMS=/home/zenoss/permissions.sql
 
 install_toolbox() {
-	pip --no-python-version-warning install /home/zenoss/zends.toolbox*whl --no-index
+	pip --no-python-version-warning --no-cache-dir install --no-index /home/zenoss/zends.toolbox*whl
 }
 
 start_db() {
@@ -27,22 +25,6 @@ fix_root_permissions() {
 		-e "UPDATE user SET plugin='mysql_native_password' WHERE user='root'; FLUSH PRIVILEGES;"
 }
 
-make_permission_sql() {
-	sed \
-		-e "s/!USER!/zenoss/g" -e "s/!PASSWD!/zenoss/g" \
-		${SQL_COMMON_IN} > ${SQL_PERMS}
-	sed \
-		-e "s/!NAME!/zodb/g" -e "s/!USER!/zenoss/g" -e "s/!PASSWD!/zenoss/g" \
-		${SQL_DB_IN} >> ${SQL_PERMS}
-	sed \
-		-e "s/!NAME!/zodb_session/g" -e "s/!USER!/zenoss/g" -e "s/!PASSWD!/zenoss/g" \
-		${SQL_DB_IN} >> ${SQL_PERMS}
-	sed \
-		-e "s/!NAME!/zenoss_zep/g" -e "s/!USER!/zenoss/g" -e "s/!PASSWD!/zenoss/g" \
-		${SQL_DB_IN} >> ${SQL_PERMS}
-	echo "FLUSH PRIVILEGES;" >> ${SQL_PERMS}
-}
-
 apply_permissions() {
 	cat ${SQL_PERMS} | mysql -u root
 }
@@ -55,7 +37,7 @@ create_databases() {
 }
 
 cleanup() {
-	rm -f ${SQL_COMMON_IN} ${SQL_DB_IN} ${SQL_PERMS} /home/zenoss/initialize_db.sh /home/zenoss/zends.toolbox*whl
+	rm -f ${SQL_PERMS} /home/zenoss/initialize_db.sh /home/zenoss/zends.toolbox*whl
 	stop_db
 }
 
@@ -68,5 +50,4 @@ start_db
 mysql_upgrade
 fix_root_permissions
 create_databases
-make_permission_sql
 apply_permissions
