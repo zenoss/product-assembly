457364ab4db77aa69463e4d1773fb3e185bfe217
Jason Peacock <jpeacock@zenoss.com>
2021-03-11 14:54:25 -0600

    fixup

--- mariadb-base/upgrade_database.sh
+++ mariadb-base/upgrade_database.sh
@@ -5,7 +5,6 @@ GLOBAL_CFG="/opt/zenoss/etc/global.conf"
 ADMIN_USER="root"
 export MYSQL_PWD=""
 
-
 case $SERVICE in
 	mariadb-model|mariadb)
 		ADMIN_USER=$(grep -r "zodb-admin-user" $GLOBAL_CFG | awk '{print $2}')
@@ -16,6 +15,9 @@ case $SERVICE in
 		MYSQL_PWD=$(grep -r "zep-admin-password" $GLOBAL_CFG | awk '{print $2}')
 		;;
 esac
+
+ZODB_USER=$(grep -r "zodb-user" $GLOBAL_CFG | awk '{print $2}')
+ZEP_USER=$(grep -r "zep-user" $GLOBAL_CFG | awk '{print $2}')
 
 start_db() {
 	chown -R mysql:mysql /var/lib/mysql
@@ -40,4 +42,4 @@ set -e
 
 start_db
 mysql_upgrade -u $ADMIN_USER
-mysql -u $ADMIN_USER < /opt/zenoss/bin/fix_permissions.sql
+mysql -u $ADMIN_USER < <(sed -e "s/!ZU!/${ZODB_USER}/g" -e "s/!EU!/${ZEP_USER}/g" /opt/zenoss/bin/fix_permissions.sql.in)
